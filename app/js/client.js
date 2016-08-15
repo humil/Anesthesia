a767bbcd6f8195e0746f404130aaa9b1f07cdcd4
Tim Ewoldt <Tim Ewoldt>
2016-08-16 00:43:23 +0200

    Initial commit
    Added webpack configuration for sass, css and js

---
+++ app/js/client.js
@@ -0,0 +1,1 @@
+require('../css/client.scss');
---
+++ app/webpack.config.js
@@ -0,0 +1,42 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var plugins = debug ? [] : [
+  new webpack.optimize.DedupePlugin(),
+  new webpack.optimize.OccurenceOrderPlugin(),
+  new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+];
+// set css bundle file
+plugins.push( new ExtractTextPlugin('css/client.min.css', {
+            allChunks: true
+        }))
+
+module.exports = {
+  context: __dirname,
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: {  "js/client": "./js/client.js" },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+        }
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('css!sass')
+      }
+    ]
+  },
+  output: {
+    path: __dirname,
+    filename: "./[name].min.js"
+  },
+  plugins: plugins,
+};
